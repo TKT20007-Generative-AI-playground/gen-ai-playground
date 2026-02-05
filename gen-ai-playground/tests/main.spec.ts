13ca79b3bf555b1c078d5b2214afbcb95699f40d
doomwall <tuomo.valkila@gmail.com>
2026-02-05 13:50:37 +0200

    changed workflow to trigger on push-main, commented out playwright tets because ui change coming.

--- gen-ai-playground/tests/main.spec.ts
+++ gen-ai-playground/tests/main.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('test main page visibility', async ({ page }) => {
+
+/* new tests coming for ui change
+ test('test main page visibility', async ({ page }) => {
   await page.goto('http://localhost:5173/');
   await expect(page.getByRole('link', { name: 'Image Generator' })).toBeVisible();
   await expect(page.getByText('Flux Kontext')).toBeVisible();
@@ -15,9 +17,9 @@ test('test text box writing and visibility', async ({ page }) => {
   await expect(page.getByText('prompt: cat in a spacesuit')).toBeVisible();
   await expect(page.getByRole('textbox', { name: 'Prompt here' })).toBeVisible();
   await expect(page.getByRole('textbox', { name: 'Prompt here' })).toHaveValue('cat in a spacesuit');
-});
+}); */
 
-test('test page has no console errors on load', async ({ page }) => {
+test('main page has no console errors on load', async ({ page }) => {
   const consoleErrors: string[] = [];
 
   page.on('console', msg => {
