14e0b87c2365dddc2cf4e0fb21c23f91e2788aa2
doomwall <tuomo.valkila@gmail.com>
2026-02-10 00:05:07 +0200

    eslint fixes, playwright fixes, test environment for frontend in docker-compose.yml

--- gen-ai-playground/frontend/src/main.tsx
+++ gen-ai-playground/frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from "./App.tsx"
 import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
-import { AuthProvider } from './context/AuthContext'
+import { AuthProvider } from './context/AuthProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
